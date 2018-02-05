c78356dc568b0ef44162c76b81dd521e87655ea7
Ishan Singh <ankit.singh040@gmail.com>
2018-02-06 02:12:18 +0530

    working routing:

--- src/App.vue
+++ src/App.vue
@@ -8,15 +8,16 @@
 
       <!--hidden-sm-and-up and hidden-xs-only used to show navigation drawer
         only on small devices like mobile-->
-      <v-toolbar-side-icon
-      @click = "sideNav=!sideNav"
-      class = "hidden-sm-and-up"></v-toolbar-side-icon>
+      <v-toolbar-side-icon @click = "sideNav=!sideNav"></v-toolbar-side-icon>
 
-      <v-toolbar-title>DevMeetup</v-toolbar-title>
+      <v-toolbar-title>
+        <!-- router-link used to link component to a page -->
+        <router-link to="/" tag="span" style="cursor: pointer">  DevMeetup </router-link>
+      </v-toolbar-title>
       <!-- due to spacer the meetup button will be on right in toolbar -->
       <!--v-toolbar-side-icon> @click = "sideNav=!sideNav"</v-toolbar-side-icon -->
       <v-spacer></v-spacer>
-      <v-toolbar-items class = "hidden-xs-only">
+      <v-toolbar-items>
         <v-btn flat v-for="item in menuItems" :key="item.title">
             <!-- left adds some spaces to the name -->
             <!-- list of icons can be found on vuetify website-->
@@ -27,7 +28,7 @@
     </v-toolbar>
 
     <!--Adds a navigation drawer-->
-    <v-navigation-drawer v-model = "sideNav">
+    <v-navigation-drawer temporary v-model = "sideNav">
         <v-list>
             <v-list-tile v-for="item in menuItems" :key="item.title">
                 <v-list-tile-action>
@@ -54,11 +55,11 @@ export default {
         //Note: Unauthenticated users should only see the Signup and Signin icon
         //menuItems will go into v-list-tile in navigation drawer
         menuItems: [
-          {icon: 'supervisor_account', title: 'View Meetups'},
-          {icon: 'room', title: 'Organize Meetup'},
-          {icon: 'person', title: 'Profile'},
-          {icon: 'face', title: 'Sign up'},
-          {icon: 'lock-open', title: 'Sign in'}
+          {icon: 'supervisor_account', title: 'View Meetups', link: '/meetups'},
+          {icon: 'room', title: 'Organize Meetup', link: '/meetup/new'},
+          {icon: 'person', title: 'Profile', link: '/profile'},
+          {icon: 'face', title: 'Sign up', link: '/signup'},
+          {icon: 'lock-open', title: 'Sign in', link: '/signin'}
         ]
     }
   },
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,18 @@
 <!-- The home page url will route to-->
 
 <template>
-    <div>
-        <p>The Home Page</p>
-    </div>
-</template>
+    <v-container fluid>
+    <v-slide-y-transition mode="out-in">
+       <v-layout column align-center>
+         <blockquote>
+           &#8220;This is the blog page &#8221;
+          <footer>
+             <small>
+               <em>&mdash;Thanks to Areeb Jamal</em>
+             </small>
+           </footer>
+         </blockquote>
+      </v-layout>
+     </v-slide-y-transition>
+   </v-container>
+ </template>
